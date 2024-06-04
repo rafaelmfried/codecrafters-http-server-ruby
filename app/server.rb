b03b36ef98523ef653253f0765535ad0cf319431
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-04 13:20:29 -0300

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -7,4 +7,12 @@ print("Logs from your program will appear here!")
 #
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
-client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+request = client_socket.gets
+_, path, _ = request.chomp.split
+
+case path
+  when "/"
+    client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
+  else
+    client_socket.puts "HTTP/1.1 404 Not Found\r\n\r\n"
+end
