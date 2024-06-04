bfbe9ea536d9d04dd891ce3eae289f1cc0e25dd1
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-04 12:27:20 -0300

    codecrafters submit [skip ci]

--- app/server.rb
+++ app/server.rb
@@ -7,3 +7,4 @@ print("Logs from your program will appear here!")
 #
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
+client_socket.puts "HTTP/1.1 200 OK\r\n\r\n"
