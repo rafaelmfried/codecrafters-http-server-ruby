dd5ecd637ae68b546e0707cc7b275fdd9aa5e55a
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-09 10:07:22 -0300

    codecrafters submit [skip ci]

--- app/http_status.rb
+++ app/http_status.rb
@@ -1,7 +1,9 @@
 class HttpStatus
   STATUS_CODES = {
     200 => 'OK',
+    201 => 'Created',
     404 => 'Not Found',
+    405 => 'Method Not Allowed',
     500 => 'Internal Server Error'
   }.freeze
 
--- app/resources/file_resource.rb
+++ app/resources/file_resource.rb
@@ -5,7 +5,6 @@ require_relative '../logger'
 class FileResource < HttpHandler
   def call(request)
     dir_flag = false
-
     directory = ''
     
     ARGV.each do |arg|
@@ -22,20 +21,38 @@ class FileResource < HttpHandler
     file_name = request.path.split('/files/').last || ''
     logger.log("Extracted string: #{file_name}")
     full_path = "#{directory}/#{file_name}"
-    file = File.exist?(full_path) ? File.open(full_path, 'r') : nil
-    logger.log("File exist?: #{file}")
-    file_content = file.read
-    logger.log("File content: #{file_content}")
-    body = file ? file_content : ''
+
+    begin
+      case request.method
+      when 'GET'
+        logger.log("Method: #{request.method}")
+        if File.exist?(full_path)
+          file_content = File.read(full_path)
+          logger.log("File content: #{file_content}")
+          headers = {
+            'Content-Type' => 'application/octet-stream',
+            'Content-Length' => file_content.size
+          }
+          response = HttpResponse.new(200, headers, file_content)
+        else
+          logger.log("File not found: #{full_path}")
+          response = HttpResponse.new(404)
+        end
+      when 'POST'
+        logger.log("Method: #{request.method}")
+        File.write(full_path, request.body)
+        response = HttpResponse.new(201)
+      else
+        logger.log("Method not supported: #{request.method}")
+        response = HttpResponse.new(405) # Method Not Allowed
+      end
+    rescue => e
+      logger.log("Error handling request: #{e.message}")
+      response = HttpResponse.new(500) # Internal Server Error
+    ensure
+      logger.log("Response: #{response.status_code} #{response.to_s}")
+    end
     
-    headers = {
-      'Content-Type' => 'application/octet-stream',
-      'Content-Length' => file ? file.size : 0
-    }
-
-    response = HttpResponse.new(200, headers, body)
-    logger.log("Response generated with body: #{body}")
-
     response
   end
 end
