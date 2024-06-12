80f5b09fcac3f4772f8b03fd3f8c35461fc05ec3
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2024-06-12 12:51:44 -0300

    codecrafters submit [skip ci]

--- app/resources/echo_resource.rb
+++ app/resources/echo_resource.rb
@@ -1,3 +1,5 @@
+require 'zlib'
+require 'stringio'
 require_relative '../http_handler'
 require_relative '../http_response'
 require_relative '../logger'
@@ -10,15 +12,37 @@ class EchoResource < HttpHandler
     str = request.path.split('/echo/').last || ''
     logger.log("Extracted string: #{str}")
 
-    body = str
-    headers = {
-      'Content-Type' => 'text/plain',
-      'Content-Length' => body.bytesize.to_s
-    }
+    accept_encoding = request.headers['Accept-Encoding'] || request.headers['accept-encoding']
+    if accept_encoding&.include?('gzip')
+      logger.log("Client accepts gzip encoding")
+      body = gzip_compress(str)
+      headers = {
+        'Content-Encoding' => 'gzip',
+        'Content-Type' => 'text/plain',
+        'Content-Length' => body.bytesize.to_s
+      }
+    else
+      logger.log("Client does not accept gzip encoding or invalid encoding provided")
+      body = str
+      headers = {
+        'Content-Type' => 'text/plain',
+        'Content-Length' => body.bytesize.to_s
+      }
+    end
 
     response = HttpResponse.new(200, headers, body)
     logger.log("Response generated with body: #{body}")
 
     response
+  end
+
+  private
+
+  def gzip_compress(str)
+    buffer = StringIO.new
+    w_gz = Zlib::GzipWriter.new(buffer)
+    w_gz.write(str)
+    w_gz.close
+    buffer.string
   end
 end
